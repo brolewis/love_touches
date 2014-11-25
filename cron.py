24aa9a87d9217b639abc47feef4cd9dcf861d447
Lewis Franklin <github@brolewis.com>
2014-11-25 00:55:48 -0500

    Testing cron to find the reason emails aren't being sent (and fixing)

--- cron.py
+++ cron.py
@@ -1,8 +1,10 @@
 # Standard Library
 import argparse
 import datetime
+import os
 # Third Party
 import pytz
+import raven
 import sqlalchemy
 import sqlalchemy.dialects.postgres
 # Local
@@ -10,45 +12,50 @@ import main
 import tasks
 
 
-def find_email(check_hour=None, check_minute=None, dry_run=False):
+def find_email(hour=None, minute=None, dry_run=False):
+    if dry_run:
+        session = main.db.create_scoped_session()
     prop = main.models.User.email_confirmed_at
-    for user in _find_user(prop, check_hour=check_hour,
-                           check_minute=check_minute):
+    for user in _find_user(prop, hour=hour, minute=minute):
         if dry_run:
+            user = session.query(main.models.User).get(user.id)
             print user.email
         else:
             tasks.send_email.delay(user.id)
 
 
-def find_phone(check_hour=None, check_minute=None, dry_run=False):
+def find_phone(hour=None, minute=None, dry_run=False):
+    if dry_run:
+        session = main.db.create_scoped_session()
     prop = main.models.User.phone_confirmed_at
-    for user in _find_user(prop, check_hour=check_hour,
-                           check_minute=check_minute):
+    for user in _find_user(prop, hour=hour, minute=minute):
         if dry_run:
+            user = session.query(main.models.User).get(user.id)
             print user.phone
         else:
             tasks.send_sms.delay(user.id)
 
 
-def _find_user(prop, check_hour=None, check_minute=None):
-    if check_hour is None:
-        check_hour = main.models.User.check_hour
-    if check_minute is None:
-        check_minute = main.models.User.check_minute
+def _find_user(prop, hour=None, minute=None):
     utc_now = datetime.datetime.utcnow()
     utc_now = utc_now.replace(tzinfo=pytz.utc)
+    if hour is not None:
+        utc_now = utc_now.replace(hour=hour)
+    if minute is not None:
+        utc_now = utc_now.replace(minute=minute)
     session = main.db.create_scoped_session()
     array = sqlalchemy.dialects.postgres.ARRAY(sqlalchemy.Integer,
                                                as_tuple=True)
     weekdays = sqlalchemy.func.array_agg(main.models.Weekday.utc_weekday,
                                          type_=array)
     user_query = session.query(main.models.User.id,
-                               main.models.User.timezone, check_hour,
+                               main.models.User.timezone,
+                               main.models.User.check_hour,
                                weekdays.label('weekdays'))
     join_clause = main.models.Weekday.user_id == main.models.User.id
     user_query = user_query.join(main.models.Weekday, join_clause)
     user_query = user_query.group_by(main.models.User.id)
-    minute_query = check_minute == utc_now.minute
+    minute_query = main.models.User.check_minute == utc_now.minute
     user_query = user_query.filter(minute_query)
     hour_list = [utc_now.hour, (utc_now - datetime.timedelta(hours=1)).hour,
                  (utc_now + datetime.timedelta(hours=1)).hour]
@@ -62,12 +69,14 @@ def _find_user(prop, check_hour=None, check_minute=None):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentPraser('Cron Job for Love Touches')
+    parser = argparse.ArgumentParser('Cron Job for Love Touches')
     parser.add_argument('--dry_run', action='store_true')
-    parser.add_argument('--check_hour', type=int)
-    parser.add_argument('--check_minute', type=int)
+    parser.add_argument('--hour', type=int)
+    parser.add_argument('--minute', type=int)
     args = parser.parse_args()
-    find_email(check_hour=args.check_hour, check_minute=args.check_minute,
-               dry_run=args.dry_run)
-    find_phone(check_hour=args.check_hour, check_minute=args.check_minute,
-               dry_run=args.dry_run)
+    try:
+        find_email(hour=args.hour, minute=args.minute, dry_run=args.dry_run)
+        find_phone(hour=args.hour, minute=args.minute, dry_run=args.dry_run)
+    except:
+        client = raven.Client(os.environ['SENTRY_DSN'])
+        client.captureException()
